8b7cefea069b9ed35c860e2d10ea26ed383e8a1e
Yashwanth Reddy Yenugu <yashwanth.reddy@abjayon.com>
2023-05-08 14:23:34 +0530

    Removed mocked data

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,18 +4,7 @@ export type User = any;
 
 @Injectable()
 export class UsersService {
-  private readonly users = [
-    {
-      userId: 1,
-      username: 'Yash',
-      password: 'Yenugu@123',
-    },
-    {
-      userId: 2,
-      username: 'Yash2',
-      password: 'Yenugu@123',
-    },
-  ];
+  private readonly users = [];
 
   async findOne(username: string): Promise<User | undefined> {
     return this.users.find((user) => user.username === username);
